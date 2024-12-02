07a4084321073460114f189f09169a2ba33cb850
Davi Nascimento <davi.nascimento0907@gmail.com>
2024-12-01 23:31:22 -0300

    Atualizando

--- JS/Integrante.js
+++ JS/Integrante.js
@@ -36,17 +36,211 @@ class Integrantes{
 }
 
 //Adicionando "itens" a classe
-Integrante1 = new Integrantes  ('Cauã Silva','Sou lindo demais para não aproveitar a vida do jeito que ela deve ser aproveitada, Sócrates ja dizia','../IMG/img_integrantes/Caua.jpg', 'https://github.com/CauaSilva28','','https://www.instagram.com/cau4_svm?igsh=cHNwa3RzMXNneWR5','', '../IMG/img_projetos_integrantes/Caua_projs/curriculum_Caua.pdf','Site Aventura', 'Descubra os melhores para se viajar com a família','https://cauasilva28.github.io/SiteAventura/', '../IMG/img_projetos_integrantes/Caua_projs/P2.png','Red Dead II', 'Descubra o mundo do melhor jogo criado em 2018','https://cauasilva28.github.io/SiteGamerRDR2/#guia','../IMG/img_projetos_integrantes/Caua_projs/P1.png','Modelagem 3D', 'Veja um dos cenários do melhor jogo de terror já desenvolvido', '../IMG/img_projetos_integrantes/Caua_projs/Finaf/html/index.html', '../IMG/img_projetos_integrantes/Caua_projs/P3.png','Aplicativo', 'Descubra um pouco mais sobre a vida de AyamiKojima', 'https://github.com/CauaSilva28/App-AyamiKojima', '../IMG/img_projetos_integrantes/Caua_projs/P4.png');
+Integrante1 = new Integrantes  (
+    'Cauã Silva',
+    'Sou lindo demais para não aproveitar a vida do jeito que ela deve ser aproveitada, Sócrates ja dizia',
+    '../IMG/img_integrantes/Caua.jpg', 'https://github.com/CauaSilva28',
+    '',
+    'https://www.instagram.com/cau4_svm?igsh=cHNwa3RzMXNneWR5',
+    '', 
+    '../IMG/img_projetos_integrantes/Caua_projs/curriculum_Caua.pdf',
+    'Site Aventura', 
+    'Descubra os melhores para se viajar com a família',
+    'https://cauasilva28.github.io/SiteAventura/', 
+    '../IMG/img_projetos_integrantes/Caua_projs/P2.png',
+    'Red Dead II', 
+    'Descubra o mundo do melhor jogo criado em 2018',
+    'https://cauasilva28.github.io/SiteGamerRDR2/#guia',
+    '../IMG/img_projetos_integrantes/Caua_projs/P1.png',
+    'Modelagem 3D', 'Veja um dos cenários do melhor jogo de terror já desenvolvido', 
+    '../IMG/img_projetos_integrantes/Caua_projs/Finaf/html/index.html', 
+    '../IMG/img_projetos_integrantes/Caua_projs/P3.png',
+    'Aplicativo', 
+    'Descubra um pouco mais sobre a vida de AyamiKojima', 
+    'https://github.com/CauaSilva28/App-AyamiKojima', 
+    '../IMG/img_projetos_integrantes/Caua_projs/P4.png'
+);
 
-Integrante2 = new Integrantes  ('Davi dos Santos','A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.','../IMG/img_integrantes/Davi.jpg', 'https://github.com/davin4sciment0','','https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1','','', 'CHARGE','Reserve aqui o melhor carro para você','https://davin4sciment0.github.io/Charge/', '../IMG/img_projetos_integrantes/Davi_projs/P1.png', 'WALL-E', 'Veja um dos personagens mais amados da Pixar', '../IMG/img_projetos_integrantes/Davi_projs/WALL-E/html/index.html', '../IMG/img_projetos_integrantes/Davi_projs/P2.png', 'PlantSP', 'Compre aqui as melhores plantas de SP', 'https://plantsp.netlify.app/carrinho', '../IMG/img_projetos_integrantes/Davi_projs/P3.png', 'APP', 'Olhe o catalago da empresa PlantSP', '','../IMG/img_projetos_integrantes/Davi_projs/P4.png');
+Integrante2 = new Integrantes  (
+    //Nome
+    'Davi dos Santos',
+    //Frase
+    'A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.',
+    //Imagem  integrante
+    '../IMG/img_integrantes/Davi.jpg', 
+    //Git
+    'https://github.com/davin4sciment0',
+    //Linkedin
+    '',
+    //Insta
+    'https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1',
+    //Whatzz
+    '',
+    //Curriculum
+    '', 
+    //Nome projeto1
+    'CHARGE',
+    //Frase projeto1
+    'Reserve aqui o melhor carro para você',
+    //Link Projeto
+    'https://davin4sciment0.github.io/Charge/', 
+    //Imagem Projeto
+    '../IMG/img_projetos_integrantes/Davi_projs/P1.png', 
+    'WALL-E', 
+    'Veja um dos personagens mais amados da Pixar', 
+    '../IMG/img_projetos_integrantes/Davi_projs/WALL-E/html/index.html', 
+    '../IMG/img_projetos_integrantes/Davi_projs/P2.png', 
+    'PlantSP', 'Compre aqui as melhores plantas de SP', 
+    'https://plantsp.netlify.app/carrinho', 
+    '../IMG/img_projetos_integrantes/Davi_projs/P3.png', 
+    'APP', 
+    'Olhe o catalago da empresa PlantSP', 
+    '',
+    '../IMG/img_projetos_integrantes/Davi_projs/P4.png'
+);
 
-Integrante3 = new Integrantes  ('Gabriel Faria','A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.','../IMG/img_integrantes/Gabriel.jpg', 'https://github.com/davin4sciment0','','https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1','', '', 'Jogo', 'Jogo desenvolvido ao longo do curso', 'https://github.com/RuanPSilva/Resgate_Dos_Animanos', '../IMG/img_projetos_integrantes/Gabriel_projs/P1.png', 'APP', 'Descubra um pouco sobre a história de Shigeru Myiamoto', 'https://github.com/GabrielFaria-cyber/Shigeru-Miyamoto', '../IMG/img_projetos_integrantes/Gabriel_projs/P2.png', 'Jogo da forca', 'Jogo desenvolvido ao longo do curso', 'https://github.com/CauaSilva28/Jogo-da-forca', '../IMG/img_projetos_integrantes/Gabriel_projs/P3.png', 'Animal TCM', 'APP desenvolvido ao longo do curso','../IMG/img_projetos_integrantes/Gabriel_projs/Animal/html/index.html', '../IMG/img_projetos_integrantes/Gabriel_projs/P4.png');
+Integrante3 = new Integrantes  (
+    'Gabriel Faria',
+    'A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.',
+    '../IMG/img_integrantes/Gabriel.jpg', 
+    'https://github.com/davin4sciment0',
+    '',
+    'https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1',
+    '', 
+    '', 
+    'Jogo', 
+    'Jogo desenvolvido ao longo do curso', 
+    'https://github.com/RuanPSilva/Resgate_Dos_Animanos', 
+    '../IMG/img_projetos_integrantes/Gabriel_projs/P1.png', 
+    'APP', 'Descubra um pouco sobre a história de Shigeru Myiamoto', 
+    'https://github.com/GabrielFaria-cyber/Shigeru-Miyamoto', 
+    '../IMG/img_projetos_integrantes/Gabriel_projs/P2.png', 
+    'Jogo da forca', 'Jogo desenvolvido ao longo do curso', 
+    'https://github.com/CauaSilva28/Jogo-da-forca', 
+    '../IMG/img_projetos_integrantes/Gabriel_projs/P3.png', 
+    'Animal TCM', 'APP desenvolvido ao longo do curso',
+    '../IMG/img_projetos_integrantes/Gabriel_projs/Animal/html/index.html', 
+    '../IMG/img_projetos_integrantes/Gabriel_projs/P4.png'
+);
 
-Integrante4 = new Integrantes  ('Fernanda Viana','A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.','../IMG/img_integrantes/Fernanda.jpg', 'https://github.com/davin4sciment0','','https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1','', '', 'Não sei');
+Integrante4 = new Integrantes  (
+    'Fernanda Viana',
+    'A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.',
+    '../IMG/img_integrantes/Fernanda.jpg', 
+    'https://github.com/davin4sciment0',
+    '',
+    'https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1',
+    '', 
+    '', 
 
-Integrante5 = new Integrantes  ('Pedro Ferraz','A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.','../IMG/img_integrantes/Pedro.jpg', 'https://github.com/davin4sciment0','','https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1','','','APP','APP sobre nosso jogo desenvolvido','https://github.com/PedroFRomao/App-Kombi-and-Dinosars', '../IMG/img_projetos_integrantes/Pedro_projs/P1.png', 'Faz ou bebe', 'APP desenvolvido para entreterimento interpessoal', 'https://github.com/PedroFRomao/App-Faz-ou-Bebe', '../IMG/img_projetos_integrantes/Pedro_projs/P2.png', 'APP', 'Descubra um pouco mais sobre a vidade Jason Chan', 'https://github.com/RuanPSilva/Jason_Chan', '../IMG/img_projetos_integrantes/Pedro_projs/P3.png', 'Poker APP','APP desenvolvido para auxilio em uma partida de poker','https://github.com/PedroFRomao/Poker_App','../IMG/img_projetos_integrantes/Pedro_projs/P4.png');
+    //Nome projeto1
+    'Jogo Animais',
+    //Frase projeto1
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto1
+    'https://github.com/RuanPSilva/Resgate_Dos_Animanos', 
+    //Imagem Projeto1
+    '../IMG/img_projetos_integrantes/Fernanda_projs/P1.png', 
 
-Integrante6 = new Integrantes  ('Ruan Pablo','A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.','../IMG/img_integrantes/Ruan.jpg', 'https://github.com/davin4sciment0','','https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1','','','LKASLKALSKAL');
+    //Nome projeto2
+    'Projeto Apex',
+    //Frase projeto2
+    'Site desenvolvido ao longo do curso',
+    //Link Projeto2
+    'https://github.com/RuanPSilva/Atividade-Aline', 
+    //Imagem Projeto2
+    '../IMG/img_projetos_integrantes/Fernanda_projs/P2.png', 
+
+    //Nome projeto3
+    'Floresta Mágica',
+    //Frase projeto3
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto3
+    'https://github.com/KauanJesusJD/Floresta_Magica', 
+    //Imagem Projeto3
+    '../IMG/img_projetos_integrantes/Ruan_projs/P3.png', 
+
+    //Nome projeto4
+    'Jogo Asteróides',
+    //Frase projeto4
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto4
+    'https://github.com/RuanPSilva/Aplicativo_Asteroides', 
+    //Imagem Projeto4
+    '../IMG/img_projetos_integrantes/Ruan_projs/P4.png', 
+);
+
+Integrante5 = new Integrantes  (
+    'Pedro Ferraz',
+    'A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.',
+    '../IMG/img_integrantes/Pedro.jpg', 
+    'https://github.com/davin4sciment0',
+    '',
+    'https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1',
+    '',
+    '',
+    'APP',
+    'APP sobre nosso jogo desenvolvido',
+    'https://github.com/PedroFRomao/App-Kombi-and-Dinosars', 
+    '../IMG/img_projetos_integrantes/Pedro_projs/P1.png', 
+    'Faz ou bebe', 'APP desenvolvido para entreterimento interpessoal', 
+    'https://github.com/PedroFRomao/App-Faz-ou-Bebe', 
+    '../IMG/img_projetos_integrantes/Pedro_projs/P2.png', 
+    'APP', 
+    'Descubra um pouco mais sobre a vidade Jason Chan', 
+    'https://github.com/RuanPSilva/Jason_Chan', 
+    '../IMG/img_projetos_integrantes/Pedro_projs/P3.png', 
+    'Poker APP',
+    'APP desenvolvido para auxilio em uma partida de poker',
+    'https://github.com/PedroFRomao/Poker_App',
+    '../IMG/img_projetos_integrantes/Pedro_projs/P4.png'
+);
+
+Integrante6 = new Integrantes  (
+    'Ruan Pablo',
+    'A vida é muito curta para deixarmos de fazer aquilo que sempre sonhamos em fazer.',
+    '../IMG/img_integrantes/Ruan.jpg', 'https://github.com/davin4sciment0',
+    '',
+    'https://www.instagram.com/davi_nasc_?utm_source=qr&igsh=ZWFtM2F1dnJ3azl1',
+    '',
+    '../IMG/img_projetos_integrantes/Ruan_projs/RuanC.pdf',
+
+    //Nome projeto1
+    'Jogo Animais',
+    //Frase projeto1
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto1
+    'https://github.com/RuanPSilva/Resgate_Dos_Animanos', 
+    //Imagem Projeto1
+    '../IMG/img_projetos_integrantes/Gabriel_projs/P2.png', 
+
+    //Nome projeto2
+    'Projeto Apex',
+    //Frase projeto2
+    'Site desenvolvido ao longo do curso',
+    //Link Projeto2
+    'https://github.com/RuanPSilva/Atividade-Aline', 
+    //Imagem Projeto2
+    '../IMG/img_projetos_integrantes/Ruan_projs/P2.png', 
+
+    //Nome projeto3
+    'Floresta Mágica',
+    //Frase projeto3
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto3
+    'https://github.com/KauanJesusJD/Floresta_Magica', 
+    //Imagem Projeto3
+    '../IMG/img_projetos_integrantes/Ruan_projs/P3.png', 
+
+    //Nome projeto4
+    'Jogo Asteróides',
+    //Frase projeto4
+    'Jogo desenvolvido ao longo do curso',
+    //Link Projeto4
+    'https://github.com/RuanPSilva/Aplicativo_Asteroides', 
+    //Imagem Projeto4
+    '../IMG/img_projetos_integrantes/Ruan_projs/P4.png', 
+);
 
 let nome = document.getElementById('nome');
 let img_integrante = document.getElementById('foto_integrante');
